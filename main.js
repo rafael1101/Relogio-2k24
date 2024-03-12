b28750d578c40f7328a698fa1f41e1e6f8f1f6ed
rafael1101 <114586875+rafael1101@users.noreply.github.com>
2024-03-12 12:34:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
   const botoes=document.querySelectorAll(".botao");
+
+ for(let i=0;i<botoes.length;i++){
+    botoes[i].onclick = function(){
+        for(let j=0;botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+ }
+
+
+
+
+
+
+
