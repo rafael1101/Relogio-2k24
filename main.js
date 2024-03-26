cf828d342b03963d4d23de96a04c78de3005b03d
rafael1101 <114586875+rafael1101@users.noreply.github.com>
2024-03-26 12:34:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
- const botoes=document.querySelectorAll(".botao");
+const botoes=document.querySelectorAll(".botao");
 const textos=document.querySelectorAll(".aba-conteudo");
 
 for(let i= 0;i < botoes.length;i++){
@@ -17,3 +17,18 @@ const tempoObjetivo1=new Date("2024-03-26T12:10:00");
 let tempoAtual =  new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal =tempoObjetivo1 - tempoAtual;
+    let segundos =Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas= Math.floor(minutos/60);
+    let dias= Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %+24;
+
+
+}
